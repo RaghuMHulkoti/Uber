d48ca13ff6c53c4a7eabd220fc06a7e60c78abc9
RaghuMHulkoti <raghavendrahulkoti1@gmail.com>
2024-12-10 22:29:32 +0530

    Captain endpoints integrated with backend

--- Frontend/src/pages/CaptainLogin.jsx
+++ Frontend/src/pages/CaptainLogin.jsx
@@ -1,16 +1,26 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { CaptainDataContext } from '../context/CaptainContext';
+import axios from 'axios';
 
 const CaptainLogin = () => {
   const [email, setemail] = useState('');
   const [password, setpassword] = useState('');
-  const [captainData, setcaptainData] = useState({});
-  const submitHandler =(e)=>{
+  const{captain, setCaptain} = React.useContext(CaptainDataContext);
+  const navigate=useNavigate();
+  const submitHandler =async(e)=>{
     e.preventDefault();
-    setcaptainData({
+   const captain={
       email:email,
       password:password
-    })
+    }
+   const response=await axios.post(`${import.meta.env.VITE_BASE_URL}/captains/login`,captain)
+   if(response.status===200){
+    const data=response.data
+    setCaptain(data.captain)
+    localStorage.setItem('token',data.token)
+    navigate('/captain-home')
+   }
     setemail('');
     setpassword('');
   }
--- Frontend/src/pages/UserProtectWrapper.jsx
+++ Frontend/src/pages/UserProtectWrapper.jsx
@@ -1,16 +1,40 @@
-import React, { useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { UserDataContext } from "../context/UserContext";
+import axios from "axios";
 
 const UserProtectWrapper = ({ children }) => {
   const navigate = useNavigate();
+  const { user, setUser } = useContext(UserDataContext)
+  const [ isLoading, setIsLoading ] = useState(true)
+  const token = localStorage.getItem('token'); // Check token inside useEffect
 
   useEffect(() => {
-    const token = localStorage.getItem('token'); // Check token inside useEffect
     if (!token) {
       navigate('/login');
+      
     }
-  }, [navigate]); // Include navigate as a dependency
-
+    axios.get(`${import.meta.env.VITE_BASE_URL}/users/profile`, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }).then(response => {
+        if (response.status === 200) {
+            setUser(response.data)
+            setIsLoading(false)
+        }
+    })
+        .catch(err => {
+            console.log(err)
+            localStorage.removeItem('token')
+            navigate('/login')
+        })
+  }, [token ]); // Include navigate as a dependency
+  if (isLoading) {
+    return (
+        <div>Loading...</div>
+    )
+}
   return <>{children}</>;
 };
 
